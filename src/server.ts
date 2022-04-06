2f706b2666573d5cb345ac95ad310d265c8d82c9
Arnon Rodrigues <arnonrdp@gmail.com>
2022-04-06 13:51:00 -0300

    Função básica de PUT

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+
 app.get("/users", (request, response) => {
   const { perPage, currentPage } = request.query;
   console.log(perPage, currentPage);
@@ -21,13 +24,21 @@ app.post("/users", (request, response) => {
   return response.json(body);
 });
 
-app.put("/users", (request, response) => {
-  return response.json({ message: "Atualizando usuário" });
+
+/**
+ * Exemplo de Rota com Query Params:
+ * http://localhost:3333/users?perPage=10&currentPage=1
+ */
+app.put("/users/:id", (request, response) => {
+  const { id } = request.params;
+  return response.json("Atualizando usuário: " + id);
 });
+
 
 app.delete("/users", (request, response) => {
   return response.json({ message: "Excluindo usuário" });
 });
+
 
 app.listen("3333", () => {
   console.log("Server started on http://localhost:3333");
