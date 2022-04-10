2d906079a8974b0411eab9cdd040a6f5b50880fd
Arnon Rodrigues <arnonrdp@gmail.com>
2022-04-10 13:51:00 -0300

    Criado métodos PUT e DELETE

--- src/server.ts
+++ src/server.ts
@@ -18,14 +18,6 @@ app.get("/users", (request, response) => {
   return response.json(users);
 });
 
-
-/**
- * Exemplo de Body de requisição:
- * {
- *   "name": "Arnon Rodrigues",
- *   "email": "arnonrdp@gmail.com"
- * }
- */
 app.post("/users", (request, response) => {
   // Recebe os dados do novo usuário
   const { name, email } = request.body;
@@ -41,15 +33,47 @@ app.post("/users", (request, response) => {
 });
 
 app.put("/users/:id", (request, response) => {
+  // Recebe os dados do usuário
   const { id } = request.params;
-  return response.json("Atualizando usuário: " + id);
+  const { name, email } = request.body;
+
+  // Localiza o usuário na base de dados
+  const userIndex = users.findIndex((user) => user.id === id);
+
+  // Se o usuário não existir, retorna um erro
+  if (userIndex < 0) {
+    return response.status(400).json({ error: "User not found" });
+  }
+
+  // Atribui os dados à uma constante para ser reutilizada
+  const user = { id, name, email };
+
+  // Atualiza o usuário na base de dados
+  users[userIndex] = user;
+
+  // Retorna os dados do usuário atualizado
+  return response.json(user);
 });
 
+app.delete("/users/:id", (request, response) => {
+  // Recebe o ID do usuário
+  const { id } = request.params;
 
-app.delete("/users", (request, response) => {
-  return response.json({ message: "Excluindo usuário" });
+  // Localiza o usuário na base de dados
+  const userIndex = users.findIndex((user) => user.id === id);
+  console.log(!!0);
+  
+  // Se o usuário não existir, retorna um erro
+  if (userIndex < 0) {
+    return response.status(400).json({ error: "User not found" });
+  }
+
+  // Remove o usuário da base de dados
+  users.splice(userIndex, 1);
+
+  // Retorna um status de sucesso
+  return response.status(204).send();
 });
-
 
 app.listen("3333", () => {
   console.log("Server started on http://localhost:3333");
